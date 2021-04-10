6136bd9cb2647af5f920039471996c1588fac9b0
Desa <dana@itu.dk>
2021-04-10 07:11:15 +0200

    Small changes in content css

--- Frontend/virtual-white-board/src/App.js
+++ Frontend/virtual-white-board/src/App.js
@@ -1,8 +1,6 @@
 
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-
-import logo from './logo.svg';
 import './App.css';
 
 import Login from './Login.js'
